b71b11735ba2db9973c82af5828a0f0f24f80bc6
nicholas lanfranca <nicholas.lanfranca@students.mq.edu.au>
2013-10-26 23:53:58 +1100

    Updated download_files.py so all item lists are downloaded.

--- code/download_files.py
+++ code/download_files.py
@@ -6,34 +6,32 @@ DIRECTORY = '.\\samples'
 
 if __name__=='__main__':
 
-    start = time.clock()
+    start = time.time()
 
     for i in client.get_item_lists():
         pathway = DIRECTORY + '\\' + i['name']
         if not os.path.exists(pathway):
             os.makedirs(pathway)
 
-        if i['name'] == 'ace':
-            items = client.get_item_list(i['item_list_url'])
-            count = 1
-            for item in items['items']:
+        items = client.get_item_list(i['item_list_url'])
+        count = 1
+        for item in items['items']:
 
-                if count % 100 == 0:
-                    print "number of documents downloaded: " + str(count)
+            if count % 100 == 0:
+                print "number of documents downloaded: " + str(count)
 
-                #need to remove '/' and ':' so it's a valid filename
-                name = item[7:].replace('/', ' ')
-                name = name.replace(':', ' ')
-                
-                filename = pathway + '\\' + name
-
-                f = open(filename, "w")
-                f.write(client.get_item_primary_text(item))
-                f.close()
-                count += 1
-
-                print filename + " has been created."
+            #need to remove '/' and ':' so it's a valid filename
+            name = item[7:].replace('/', ' ')
+            name = name.replace(':', ' ')
             
+            filename = pathway + '\\' + name
+            
+            f = open(filename, "w")
+            f.write(client.get_item_primary_text(item))
+            f.close()
 
+            count += 1
 
-    print (time.clock() - start)
+            print filename + " has been created."
+            
+    print (time.time() - start)
