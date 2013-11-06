c189f24f958741f49fb55ae614bff20ea73a7a4d
nicholas lanfranca <nicholas.lanfranca@students.mq.edu.au>
2013-11-06 20:58:26 +1100

    Added a list for positions

--- code/index_value.py
+++ code/index_value.py
@@ -5,12 +5,19 @@ class IndexValue:
         self._token = token
         self._filename = filename
         self._char_offset_values = []
+        self._position = []
 
-    def add_char_offset_value(self, char_offset_value):
+    def add_char_offset_values(self, char_offset_value):
         self._char_offset_values.extend(char_offset_value)
 
     def get_char_offset_values(self):
         return self._char_offset_values
+
+    def add_position(self, position):
+        self._position.extend(position)
+
+    def get_position(self):
+        return self._position
 
     def get_filename(self):
         return self._filename
